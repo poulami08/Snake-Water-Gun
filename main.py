94a4c3115b4b4b9d203bb64dddc7add47b06afa5
Poulami Mandal <poulami.kv@gmail.com>
2021-09-27 15:17:00 +0530

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 
-print("                                                  ****** SNAKE, WATER AND GUN *******                  ")
-print(" ")
+print("Hey! Welcome to snake, water, gun game")
 a=0
 b=0
 while(True):
@@ -19,7 +18,7 @@ while(True):
     player =m.lower()
     if player =="snake" or player =="water" or player =="gun":
         if computer == player:
-            print("Tie match") 
+            print("the game is a tie") 
             a+=1
             b+=1
         elif computer =="snake":
@@ -50,13 +49,62 @@ while(True):
         
     print(f"Player 1 choose {computer}")
     print(f"Player 2 choose {player}")
-print(f"Score of player 1 :{a}")
-print(f"Score of player 2 :{b}")
-if a>b:
-    print("Congratulations! Player 1 won the game")
-elif b>a:
-    print("Congratulations! Player 2 won the game")
+print(f"score of player 1 :{a}")
+print(f"score of player 2 :{b}")
+
+import random
+
+
+def game(computer,player):
+   
+        
+        
+            if computer == player:
+                return None
+            elif computer =="snake":
+                if player =="water":
+                    return False
+                elif player =="gun":
+                    return True
+            elif computer =="water":
+                if player =="gun":
+                    return False
+                elif player =="snake":
+                    return True
+            elif computer =="gun":
+                if player =="snake":
+                    return False
+                elif player =="water":
+                    return True
+        
+        
+
+
+print("Hey! Welcome to snake, water, gun game")
+
+print("Player 1- choose an object : snake , water or gun? ")
+rand = random.randint(1,3)
+if rand == 1:
+    computer ="snake"
+elif rand == 2:
+    computer = "water"
+elif rand == 3:
+    computer = "gun"
+print("Player 2- choose an object : snake , water or gun?")
+m=input()
+player =m.lower()
+
+
+
+
+print(f"Player 1 choose {computer}")
+print(f"Player 2 choose {player}")
+
+a= game(computer,player)
+
+if a == None:
+    print("the game is a tie")
+elif a == True:
+    print("You win")
 else:
-    print("Congratulations!  Both player 1 and player 2 won the game")
-print(" ")
-print("                                                        ***** GAME OVER ******                 ")
+    print("You lose")
